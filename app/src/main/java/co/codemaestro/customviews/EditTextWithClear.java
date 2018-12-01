11ea13e7f4489c2f9b3b12c8fcf5d296f8acf08f
Mitch Greene <m.iangreene@gmail.com>
2018-11-30 21:20:55 -0500

    Day 068 of 100DoCC

--- app/src/main/java/co/codemaestro/customviews/EditTextWithClear.java
+++ app/src/main/java/co/codemaestro/customviews/EditTextWithClear.java
@@ -3,7 +3,11 @@ package co.codemaestro.customviews;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.support.v4.content.res.ResourcesCompat;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.EditText;
 
 public class EditTextWithClear extends android.support.v7.widget.AppCompatEditText {
@@ -27,5 +31,78 @@ public class EditTextWithClear extends android.support.v7.widget.AppCompatEditTe
 
     private void init() {
         clearButtonImage = ResourcesCompat.getDrawable(getResources(), R.drawable.ic_clear_opaque_24dp, null);
+        addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                showClearButton();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if ((getCompoundDrawablesRelative()[2] != null)) {
+                    float clearButtonStart;
+                    float clearButtonEnd;
+                    boolean isClearButtonClicked = false;
+
+                    // Detech the touch in RTL or LTR layout direction
+                    if(getLayoutDirection() == LAYOUT_DIRECTION_RTL) {
+                        clearButtonEnd = clearButtonImage.getIntrinsicWidth() + getPaddingStart();
+                        if(event.getX() < clearButtonEnd) {
+                            isClearButtonClicked = true;
+                        }
+
+                    } else {
+                        clearButtonStart = (getWidth() - getPaddingEnd() - clearButtonImage.getIntrinsicWidth());
+                        if(event.getX() > clearButtonStart) {
+                            isClearButtonClicked = true;
+                        }
+                    }
+
+                    // Check for actions if the button is tapped
+                    if(isClearButtonClicked) {
+                        // Check for action down
+                        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                            clearButtonImage = ResourcesCompat.getDrawable(getResources(), R.drawable.ic_clear_black_24dp, null);
+                            showClearButton();
+                        }
+                        // Check for action up
+                        if(event.getAction() == MotionEvent.ACTION_UP) {
+                            clearButtonImage = ResourcesCompat.getDrawable(getResources(), R.drawable.ic_clear_opaque_24dp, null);
+                            getText().clear();
+                            hideClearButton();
+                            return true;
+                        }
+                    } else {
+                        return false;
+                    }
+                }
+
+
+                return false;
+            }
+        });
+    }
+
+
+
+    //Shows the clear X Button
+    private void showClearButton() {
+        setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, clearButtonImage, null);
+    }
+
+    private void hideClearButton() {
+        setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, null, null);
     }
 }
